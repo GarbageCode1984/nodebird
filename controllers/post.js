a6cfe6f8ba14625a7bb44bb4e0eec363251e2f46
GarbageCode <rkqlwl1984@gmail.com>
2025-09-22 02:49:46 +0900

    post 라우터 설정

---
+++ controllers/post.js
@@ -0,0 +1,19 @@
+const { Post, Hashtag } = require("../models");
+
+exports.afterUploadImage = (req, res) => {
+    console.log(req.file);
+    res.json({ url: `/img/${req.file.filename}` });
+};
+
+exports.uploadPost = async (req, res, next) => {
+    try {
+        const post = await Post.create({
+            content: req.body.content,
+            img: req.body.url,
+            UserId: req.user.id,
+        });
+        const hashtags = req.body.content.match(/#[^\s#]*/g);
+        if (hashtags) {
+        }
+    } catch (error) {}
+};
